4883989f60569a78acd50d9f460c540c0686c989
morri jabbie <mahamadoujabbie838@gmail.com>
2025-10-13 15:26:58 +0000

    my netlify

---
+++ netlify/functions/app.js
@@ -0,0 +1,21 @@
+//setting up express and serverless-http
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+
+//Middleware
+app.use(express.json());
+
+//routes
+app.get('/', (reg, res)=>{
+    res.json({
+        message:"hello my developers"
+    })
+});
+
+app.get('/user', (reg, res)=>{
+    res.status(200).json([{user: "morri"},{user:"lamin"}])
+});
+
+//exporting express
+module.exports.handler = serverless(app);
