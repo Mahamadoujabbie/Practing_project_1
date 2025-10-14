7906524a10e0df21215ab49f75165b795e03ae56
mahamadou jabbie <mahamadoujabbie838@gmail.com>
2025-10-14 22:41:16 +0000

    i add json.js file

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -1,7 +1,8 @@
 //setting up express and serverless-http
-const express = require('express');
-const serverless = require('serverless-http');
-const cors = require('cors');
+const express = require("express");
+const serverless = require("serverless-http");
+const images = require("./json");
+const cors = require("cors");
 const app = express();
 
 //Middleware
@@ -9,14 +10,14 @@ app.use(express.json());
 app.use(cors());
 
 //routes
-app.get('/', (reg, res)=>{
-    res.json({
-        message:"hello my developers"
-    })
+app.get("/", (reg, res) => {
+  res.json({
+    message: "hello my developers",
+  });
 });
 
-app.get('/user', (reg, res)=>{
-    res.status(200).json([{user: "morri"},{user:"lamin"}])
+app.get("/images", (reg, res) => {
+  res.status(200).json(images);
 });
 
 //exporting express
